09b42ad5d7b091908a85bc40118397a98e169102
tahrijouteyamer <tahrijouteyamer@hotmail.com>
2024-02-27 00:41:24 +0000

    .

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -3,7 +3,8 @@
     flask with general routes
     routes:
         /status:    display "status":"OK"
-        /stats:     dispaly total for all classes
+        /stats:     display total for all classes
+        /api/v1/stats: display count of each object type
 '''
 from api.v1.views import app_views
 from flask import jsonify
@@ -32,3 +33,20 @@ def storage_counts():
         "users": storage.count("User")
     }
     return jsonify(cls_counts)
+
+
+@app_views.route("/stats", methods=['GET'], strict_slashes=False)
+def object_counts():
+    '''
+        return count of each object type
+    '''
+    obj_counts = {
+        "amenities": storage.count("Amenity"),
+        "cities": storage.count("City"),
+        "places": storage.count("Place"),
+        "reviews": storage.count("Review"),
+        "states": storage.count("State"),
+        "users": storage.count("User")
+    }
+    return jsonify(obj_counts)
+
